6769e5853fa24859cb65fac71473c02ff37584a6
drhoto200 <david_rhoton@comcast.com>
2020-06-03 11:21:14 -0400

    initial commit

---
+++ src/main/java/org/launchcode/tattoo_finder/TattooFinderApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.tattoo_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattooFinderApplication.class, args);
+	}
+
+}
