6b60986aedaac70bb34f9e15c988a1669f31b8a3
drhoto200 <david_rhoton@comcast.com>
2020-06-12 11:58:53 -0400

    Image upload Added

--- src/main/java/org/launchcode/tattoo_finder/controllers/SearchController.java
+++ src/main/java/org/launchcode/tattoo_finder/controllers/SearchController.java
@@ -2,6 +2,7 @@ package org.launchcode.tattoo_finder.controllers;
 
 import org.launchcode.tattoo_finder.models.Artist;
 import org.launchcode.tattoo_finder.models.ArtistInfo;
+import org.launchcode.tattoo_finder.models.User;
 import org.launchcode.tattoo_finder.models.data.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import static org.launchcode.tattoo_finder.controllers.HomeController.columnChoices;
 
 @Controller
@@ -18,8 +23,18 @@ public class SearchController {
     @Autowired
     private ArtistRepository artistRepository;
 
+    @Autowired
+    AuthenticationController authenticationController;
+
     @RequestMapping("")
-    public String search(Model model) {
+    public String search(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        model.addAttribute("role", user.getRole());
+        model.addAttribute("isLoggedIn", (user != null));
+
         model.addAttribute("columns", columnChoices);
         return "search";
     }
--- src/main/java/org/launchcode/tattoo_finder/controllers/StyleController.java
+++ src/main/java/org/launchcode/tattoo_finder/controllers/StyleController.java
@@ -1,6 +1,7 @@
 package org.launchcode.tattoo_finder.controllers;
 
 import org.launchcode.tattoo_finder.models.Style;
+import org.launchcode.tattoo_finder.models.User;
 import org.launchcode.tattoo_finder.models.data.StyleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -18,8 +21,18 @@ public class StyleController {
     @Autowired
     StyleRepository styleRepository;
 
+    @Autowired
+    AuthenticationController authenticationController;
+
     @GetMapping
-    public String displayAllStyles(Model model) {
+    public String displayAllStyles(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        model.addAttribute("role", user.getRole());
+        model.addAttribute("isLoggedIn", (user != null));
+
         model.addAttribute("styles", styleRepository.findAll());
         return "styles/index";
     }
